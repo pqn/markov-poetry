1ed3960954540389672c60a632f670cc88404133
Prem Nair <prem.q.nair@gmail.com>
2015-09-20 05:17:45 -0400

    Single page app now

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, jsonify
 from poetry import markov_poem
 
 app = Flask(__name__)
@@ -25,6 +25,25 @@ def poem():
     txtName = file.read()
     txtName = markov_poem(txtName)
     return render_template('form_action.html', txtName=txtName)
+
+@app.route('/poem2', methods=['POST'])
+def poem2():
+    txtName = request.form['txt']
+    if txtName == '0':
+        file = open("amendments.txt")
+    elif txtName == '1':
+        file = open("moby_dick.txt")
+    elif txtName == '2':
+        file = open("aesop.txt")
+    elif txtName == '3':
+        file = open("beatles.txt")
+    elif txtName == '4':
+        file = open("bible.txt")
+    else:
+        file = open("amendments.txt")
+    txtName = file.read()
+    txtName = markov_poem(txtName)
+    return jsonify({"resp" : txtName})
 
 # Run the app :)
 if __name__ == '__main__':
