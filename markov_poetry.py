c6e6cb94c342796b0de550dadb0e8f8d0301c4cc
Prem Nair <prem.q.nair@gmail.com>
2015-09-19 20:46:31 -0400

    Reverse the Markov, change default fallback to random choice

--- markov_poetry.py
+++ markov_poetry.py
@@ -17,7 +17,7 @@ def markov(triples_list):
     return chain
 
 def finalize_markov(markov_dict):
-    final_chain = defaultdict(lambda: ((1,), ("the",)))
+    final_chain = dict()
     for pair, third_word_dict in markov_dict.items():
         values = []
         total = 0
@@ -28,19 +28,23 @@ def finalize_markov(markov_dict):
     return final_chain
     
 def getWords(text):
-    return re.compile('\w+').findall(text)
+    return re.compile('[\w\']+').findall(text)
 
-def weighted_rand_choice(cum_values, choices):
+def weighted_rand_choice(A):
+    (cum_values, choices) = A
     i = bisect(cum_values, random.random() * cum_values[-1])
     return choices[i]
 
 def generate(final_chain, num_words):
     seed = list(random.choice(list(final_chain.keys())))
     for x in range(num_words - 2):
-        seed.append(weighted_rand_choice(*final_chain[(seed[-2], seed[-1])]))
+        seed.append(weighted_rand_choice(final_chain.get((seed[-2], seed[-1]), random.choice(list(final_chain.values())))))
     return seed
 
 with open("test.txt") as f:
     text = f.read()
     words = [x.lower() for x in getWords(text)]
-    print(generate(finalize_markov(markov(triples(words))), 100))
+    words.reverse()
+    result = generate(finalize_markov(markov(triples(words))), 100)
+    result.reverse()
+    print(result)
